ee1f7e6943b393cfe4a0dab6c16ece11ce327348
aperiodic <dlp@aperiodic.org>
2015-01-02 11:18:47 -0800

    Add clj-http & cheshire to dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,13 @@
             :url "https://gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [org.clojure/clojurescript "0.0-2411"]
+                 [cheshire "5.4.0"]
+                 [clj-http "1.0.1"]
                  [org.clojars.franks42/cljs-uuid-utils "0.1.3"]
                  [jamesmacaulay/zelkova "0.2.0"]
                  [om "0.7.3"]]
   :profiles {:dev {:plugins [[lein-cljsbuild "1.0.3"]]}}
-  :source-paths ["src/cljs"]
+  :source-paths ["src/cljs" "src/clj"]
   :target-path "target/jvm/%s"
   :cljsbuild {:builds [{:id "arcane-lab"
                         :source-paths ["src/cljs"]
