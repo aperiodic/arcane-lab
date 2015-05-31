0f8e983a749d550a8dcfccdc77581d8576bfd1ed
aperiodic <dlp@aperiodic.org>
2015-05-31 13:23:41 -0700

    Add multi-source image proxying.
    Add the `arcane-lab.images` namespace, which contains a route that
    serves card images by multiverse ID. The card images can come from
    either MTG DB or Gatherer; MTG DB's high-res images are preferred, then
    the lo-res, then Gatherer as a last resort. The source from which an
    image was successfully retrieved is cached, so further lookups will have
    much less latency.
    
    The HTTP "Last-Modified" header is added to every response, and if the
    client sends an "If-Modified-Since" header, then a 304 is returned
    (always -- we assume images never get updated).
    
    Together, these should allow us to always find the best possible card
    image for a multiverse ID, with reasonable performance and minimal data
    transfer.

--- src/clj/arcane_lab/images.clj
+++ src/clj/arcane_lab/images.clj
@@ -1,27 +1,28 @@
 (ns arcane-lab.images
   (:import org.apache.http.cookie.MalformedCookieException)
-  (:require [compojure.core :refer [routes GET]]
+  (:require [arcane-lab.utils :refer [now-rfc822]]
+            [compojure.core :refer [routes GET]]
             [puppetlabs.http.client.sync :as http]
             [ring.middleware.not-modified :refer [wrap-not-modified]]))
 
-(def source-ordering [:mtgdb-hi :mtgdb-lo :gatherer])
+(def sources [:mtgdb-hi :mtgdb-lo :gatherer])
+(def startup-time (now-rfc822))
 
-(comment (.indexOf source-ordering :mtgdb-lo))
-
-(def sources
-  (sorted-map-by #(< (.indexOf source-ordering %1) (.indexOf source-ordering %2))
+(def source->url-fn
+  (sorted-map-by #(< (.indexOf sources %1) (.indexOf sources %2))
     :mtgdb-hi (fn [m-id] (str "http://api.mtgdb.info/content/hi_res_card_images/" m-id ".jpg"))
     :mtgdb-lo (fn [m-id] (str "http://api.mtgdb.info/content/card_images/" m-id ".jpg"))
-    :gatherer (fn [m-id] (str "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid="
+    :gatherer (fn [m-id] (str "http://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid="
                               m-id))))
 
 (def acceptable-status? #{304 200})
 
-(defn image-from-source
-  [source multiverse-id]
-  {:pre [(contains? sources source)]}
-  (let [m-id->src (get sources source)]
-    (http/get (m-id->src multiverse-id))))
+(defn get-card-image
+  [source m-id req]
+  {:pre [(contains? source->url-fn source)]}
+  (if (get-in req [:headers "if-modified-since"])
+    {:status 304, :headers {"content-type" "text/html"}} ;; always 304 if they seem to have it
+    (http/get ((source->url-fn source) m-id))))
 
 (defn successful-gatherer-response?
   [resp]
@@ -29,10 +30,10 @@
       (not (re-find #"^http://gatherer\.wizards\.com" (get-in resp [:opts :url])))))
 
 (defn image-and-source
-  [multiverse-id]
-  (loop [sources source-ordering]
+  [multiverse-id req]
+  (loop [sources sources]
     (if-let [source (first sources)]
-      (let [{:keys [status] :as resp} (image-from-source source multiverse-id)]
+      (let [{:keys [status] :as resp} (get-card-image source multiverse-id req)]
         (if (and (acceptable-status? status) (successful-gatherer-response? resp))
           [source resp]
           (recur (next sources))))
@@ -40,37 +41,26 @@
       [nil {:status 404
             :body (str "Could not find image for multiverse-id")}])))
 
-(comment
-
-  (let [img (best-image 699)
-        {{:strs [content-type last-modified]} :headers} img]
-    (clojure.pprint/pprint (:headers img))
-    )
-
-  (-> (image-and-source 342564333333333)
-    second
-    )
-  )
-
 (defn source-memoized-best-image
-  [an-atom]
-  (fn [multiverse-id]
-    (if-let [source (get @an-atom multiverse-id)]
-      (http/get )
-      (let [result (best-image multiverse-id)]
-        ))))
+  [!cache]
+  (fn [multiverse-id req]
+    (-> (if-let [source (get @!cache multiverse-id)]
+          (get-card-image source multiverse-id req)
+          (let [[source result] (image-and-source multiverse-id req)]
+            (swap! !cache assoc multiverse-id source)
+            result))
+      (update-in [:headers "last-modified"] (fnil identity startup-time)))))
 
 (defn image-routes
   ([] (image-routes (atom {})))
-  ([memoize ]
-   (let [best-image (memoize best-image)]
-    (-> (GET "/img/:m-id" [m-id]
-             (let [{:keys [status headers body]} (best-image m-id)
+  ([!cache]
+   (let [best-image (source-memoized-best-image !cache)]
+    (GET "/:m-id" [m-id :as req]
+             (let [{:keys [status headers body]} (best-image m-id req)
                    {:strs [content-type content-length last-modified]} headers]
                (cond-> {:status status
                         :body body}
                  (acceptable-status? status)
-                 (assoc :headers {:content-type content-type
-                                  :content-length content-length
-                                  :last-modified last-modified}))))
-      (wrap-not-modified)))))
+                 (update-in [:headers] (partial merge {"content-type" content-type
+                                                       "content-length" content-length
+                                                       "last-modified" last-modified}))))))))
