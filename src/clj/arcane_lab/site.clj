483d14aa542ed4989e77c27af68db83714d61ea0
aperiodic <dlp@aperiodic.org>
2016-03-30 22:19:58 -0700

    Make SOI the default format

--- src/clj/arcane_lab/site.clj
+++ src/clj/arcane_lab/site.clj
@@ -36,7 +36,7 @@
   ([] (site-routes (atom-bucket/init)))
   ([bucket]
    (routes
-     (GET "/" [] (resp/redirect "/4OGW2BFZ"))
+     (GET "/" [] (resp/redirect "/6SOI"))
 
      (GET "/decks/:deck-hash" req
           (cached-html-resp (resp/resource-response "/index.html") req))
