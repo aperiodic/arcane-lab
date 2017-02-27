f0beac6beb5ebdd800b6bcad6d9173595a1617a8
aperiodic <dlp@aperiodic.org>
2017-02-26 18:11:26 -0800

    Add binary-search to util ns

--- src/cljs/arcane_lab/util.cljs
+++ src/cljs/arcane_lab/util.cljs
@@ -7,3 +7,29 @@
 (defn mean
   [x y]
   (-> (+ x y) (/ 2)))
+
+(defn mean-int
+  [x y]
+  (-> (+ x y) (/ 2) int))
+
+(defn binary-search
+  "Given a sorted collection and a number, return the index at which the number
+  would be inserted in the collection."
+  ([xs x] (let [n (count xs)]
+            (cond
+              (<= n 0) 0
+              (= n 1) (if (<= x (nth xs 0)) 0 1)
+              :else (binary-search xs x 0 n))))
+  ([xs x i_min i_max]
+   (if (= i_min i_max)
+     i_min
+     (let [i_mean (mean-int i_min i_max)
+           comparison (compare x (nth xs i_mean))]
+       (cond
+         (neg? comparison) (if (= i_min i_mean)
+                             i_min
+                             (recur xs x i_min i_mean))
+         (pos? comparison) (if (= i_min i_mean)
+                             (recur xs x (inc i_mean) i_max)
+                             (recur xs x i_mean i_max))
+         (zero? comparison) i_mean)))))
