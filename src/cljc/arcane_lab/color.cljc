09575a2b236370cfa61ecade5e7e1473e2da8d2c
aperiodic <dlp@aperiodic.org>
2017-05-06 12:41:44 -0700

    Break out main's color fns into arcane-lab.color
    Move the color-related functions defined in the main clojurescript
    namespace to the cljc namespace `arcane-lab.color`, where it can be used
    by both `.clj` and `.cljs` namespaces.

---
+++ src/cljc/arcane_lab/color.cljc
@@ -0,0 +1,49 @@
+(ns arcane-lab.color)
+
+(def color-order [:white :blue :black :red :green :gold :colorless])
+
+(def color->index
+  {:white 0
+   :blue 1
+   :black 2
+   :red 3
+   :green 4})
+
+(defn wubrggc-sort
+  "Sort cards by WUBRG, with gold cards followed by colorless ones at the end."
+  [card]
+  (let [{:keys [colors]} card
+        color-count (count colors)]
+    (cond
+      (zero? color-count) 6
+      (> (count colors) 1) 5
+      :otherwise (color->index (first colors) 0))))
+
+(defn cost->colortype
+  "Return the color type indicated by a mana cost. If the cost contains only one
+  color, return that color as a keyword. If it contains multiple colors, then
+  return `:gold`. If the cost is coloress, return nil."
+  [mana-cost]
+  (if mana-cost
+    (let [w-cost? (re-find #"W" mana-cost)
+          u-cost? (re-find #"U" mana-cost)
+          b-cost? (re-find #"B" mana-cost)
+          r-cost? (re-find #"R" mana-cost)
+          g-cost? (re-find #"G" mana-cost)
+          gold-cost? (or (and w-cost? u-cost?)
+                         (and u-cost? b-cost?)
+                         (and b-cost? r-cost?)
+                         (and r-cost? g-cost?)
+                         (and g-cost? w-cost?)
+                         (and w-cost? b-cost?)
+                         (and u-cost? r-cost?)
+                         (and b-cost? g-cost?)
+                         (and r-cost? w-cost?)
+                         (and g-cost? u-cost?))]
+      (cond
+        gold-cost? :gold
+        w-cost? :white
+        u-cost? :blue
+        b-cost? :black
+        r-cost? :red
+        g-cost? :green))))
--- src/cljs/arcane_lab/main.cljs
+++ src/cljs/arcane_lab/main.cljs
@@ -1,6 +1,7 @@
 (ns arcane-lab.main
   (:require [ajax.core :as async-http]
             [ajax.edn :refer [edn-response-format]]
+            [arcane-lab.color :as color]
             [arcane-lab.constants :as c]
             [arcane-lab.history :as history]
             [arcane-lab.piles :as piles]
@@ -40,25 +41,6 @@
                       :selected? false)
       reverse-side (update :reverse add-img-src))))
 
-(def color-order [:white :blue :black :red :green :gold :colorless])
-
-(def color->index
-  {:white 0
-   :blue 1
-   :black 2
-   :red 3
-   :green 4})
-
-(defn wubrggc-sort
-  "Sort cards by WUBRG, with gold cards followed by colorless ones at the end."
-  [card]
-  (let [{:keys [colors]} card
-        color-count (count colors)]
-    (cond
-      (zero? color-count) 6
-      (> (count colors) 1) 5
-      :otherwise (color->index (first colors) 0))))
-
 (defn basic-land?
   [card]
   (let [basic-land-names #{"Plains" "Island" "Swamp" "Mountain" "Forest"}]
@@ -67,35 +49,6 @@
 (defn rare?
   [card]
   (contains? #{:rare :mythic-rare} (:rarity card)))
-
-(defn cost->colortype
-  "Return the color type found by looking at the mana cost only. If the cost
-  contains only one color, return that color as a keyword. If it contains
-  multiple colors, then return `:gold`. If the cost is coloress, return nil."
-  [mana-cost]
-  (if mana-cost
-    (let [w-cost? (re-find #"W" mana-cost)
-          u-cost? (re-find #"U" mana-cost)
-          b-cost? (re-find #"B" mana-cost)
-          r-cost? (re-find #"R" mana-cost)
-          g-cost? (re-find #"G" mana-cost)
-          gold-cost? (or (and w-cost? u-cost?)
-                         (and u-cost? b-cost?)
-                         (and b-cost? r-cost?)
-                         (and r-cost? g-cost?)
-                         (and g-cost? w-cost?)
-                         (and w-cost? b-cost?)
-                         (and u-cost? r-cost?)
-                         (and b-cost? g-cost?)
-                         (and r-cost? w-cost?)
-                         (and g-cost? u-cost?))]
-      (cond
-        gold-cost? :gold
-        w-cost? :white
-        u-cost? :blue
-        b-cost? :black
-        r-cost? :red
-        g-cost? :green))))
 
 ;;
 ;; Interface Hackery
@@ -167,9 +120,10 @@
         rare->pile (fn [i rare]
                      (piles/make-pile
                        [rare] (piles/x-of-column-indexed i) c/half-gutter))
-        rare-piles (map-indexed rare->pile (sort-by wubrggc-sort rares))
+        rare-piles (map-indexed rare->pile (sort-by color/wubrggc-sort rares))
+        cost->col color/cost->colortype
         color->non-rares (group-by (fn [{:keys [colors manaCost] :as card}]
-                                     (if-let [cost-color (cost->colortype manaCost)]
+                                     (if-let [cost-color (cost->col manaCost)]
                                        cost-color
                                        (cond
                                          (empty? colors) :colorless
@@ -187,7 +141,7 @@
                                     cs-&-ps
                                     (conj cs-&-ps [color x]))))
                               []
-                              color-order)
+                              color/color-order)
         pile-for-color-at-x (fn [[color x]]
                               (let [col-cards (color->non-rares color)
                                     y (+ c/half-gutter c/card-height c/gutter)]
