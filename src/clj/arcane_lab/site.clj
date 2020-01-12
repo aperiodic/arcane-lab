fee6f39ff13d36c5cd10035c5adf308563324eb5
aperiodic <dlp@aperiodic.org>
2020-01-11 23:46:32 -0800

    Make RNA,WAR,M20,ELD public (I've been slacking).

--- src/clj/arcane_lab/site.clj
+++ src/clj/arcane_lab/site.clj
@@ -38,7 +38,7 @@
   ([] (site-routes (atom-bucket/init)))
   ([bucket]
    (routes
-     (GET "/" [] (resp/redirect "/6GRN"))
+     (GET "/" [] (resp/redirect "/6ELD"))
 
      (GET "/decks/:deck-hash" req
           (cached-html-resp (resp/resource-response "/index.html") req))
