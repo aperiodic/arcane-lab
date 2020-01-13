ce0b16419277459ea221bc0b215d111fcf8c7378
aperiodic <dlp@aperiodic.org>
2020-01-12 17:53:54 -0800

    Add fns to trim MTGJSON's AllPrintings file.
    By throwing out sets without a :boosterV3 field, and trimming away all
    the big per-set and per-card fields that we don't use (such as each
    set's tokens, or all the foreign versions or printings of each card), we
    can reduce this file by over 80%, from 165 to 31 MB!
    
    This will make a lot of things smaller:
      * the size of update commits;
      * the size of the uberjar I upload from my home connection;
      * baseline memory usage;
    The first two should make updating to add new sets a bit smoother, and
    the last one will make more room for the image cache in production and
    hopefully make my REPL OOM less often.
    
    I've gone back and used these functions to rewrite the two commits where
    I updated `resources/cards-by-set.json` to have all the stuff from
    MTGJSON's `AllPrintings.json`, so now those commits add the trimmed
    versions instead.

--- src/clj/arcane_lab/data.clj
+++ src/clj/arcane_lab/data.clj
@@ -1,10 +1,14 @@
 (ns arcane-lab.data
   "Functions for manipulating the JSON data from the MtGJSON project, e.g. for
   merging a standalone set file into all-sets file."
-  (:require [cheshire.core :as json]
+  (:require [arcane-lab.sets :as sets]
+            [cheshire.core :as json]
             [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]]
             [clojure.set :as set]))
+
+(def CARDS_BY_SET_FILENAME "cards-by-set.json")
+(def CARDS_BY_SET_FILEPATH (str "resources/" CARDS_BY_SET_FILENAME))
 
 (defn parse-json-resource
   [resource-name]
@@ -24,11 +28,49 @@
           :foreignNames :originalText :originalType :printings))
 
 (defn all-sets []
-  (parse-json-resource "cards-by-set.json"))
+  (parse-json-resource CARDS_BY_SET_FILENAME))
 
 (defn eldritch-moon []
   (-> (parse-json-resource "EMN.json")
     (update :cards (partial map set-card->all-sets-card))))
+
+(defn map-over-sets
+  [f all-sets]
+  (into {} (for [[code mtg-set] all-sets]
+             [code (f mtg-set)])))
+
+(defn filter-sets
+  [f all-sets]
+  (into {} (for [[code mtg-set] all-sets
+                 :when (f mtg-set)]
+             [code mtg-set])))
+
+(defn map-over-cards
+  [f all-sets]
+  (map-over-sets (fn [s] (update s :cards #(mapv f %)))
+                 all-sets))
+
+(defn trim-set
+  [mtg-set]
+  (dissoc mtg-set :tokens :translations))
+
+(defn trim-card
+  [card]
+  (dissoc card
+          :flavorText, :foreignData, :legalities, :prices, :printings
+          :purchaseUrls, :rulings))
+
+(defn trim-all-sets
+  [all-sets]
+  (->> all-sets
+    (filter-sets :boosterV3)
+    (map-over-sets trim-set)
+    (map-over-cards trim-card)))
+
+(defn -main
+  [& _]
+  (let [trimmed-sets (trim-all-sets (all-sets))]
+    (spit CARDS_BY_SET_FILEPATH (json/encode trimmed-sets))))
 
 (comment
   (def emn (eldritch-moon))
